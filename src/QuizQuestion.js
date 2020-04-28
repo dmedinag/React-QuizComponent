386f80ce52a74e1ffab37be58ae248e06d052552
Dani Medina <dmedinag@pm.me>
2020-04-28 22:38:39 +0200

    adding error message when answer is incorect

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,17 +4,23 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 class QuizQuestion extends Component {
   constructor(props) {
     super(props)
+    this.state = { incorrectAnswer: false }
   }
 
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
+      this.state = { incorrectAnswer: false }
       this.props.showNextQuestionHandler()
+    } else {
+      this.state = { incorrectAnswer: true }
     }
   }
 
   render() {
     return (
       <main>
+        {this.state.incorrectAnswer
+          ? <p className='error'>Sorry, that's not right</p> : null}
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
